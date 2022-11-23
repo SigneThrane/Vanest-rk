5dc0b62510b4d630baa2738aa4fea06c5b703372
AChristineP <94613529+AChristineP@users.noreply.github.com>
2022-11-23 15:24:52 +0100

    Footer

--- footer.php
+++ footer.php
@@ -6,18 +6,105 @@
  *
  * @package storefront
  */
-
 ?>
 
-<!-- Footer -->
-<footer class="text-center text-black" style="background-color: #CB997E;">
+</div>
+	</div>
 
+	<?php do_action( 'storefront_before_footer' ); ?>
+  <!-- Copyright -->
+  <div class="text-center" id="footerstyle">
+  <div class="row">
+    <div class="col">
+	<p><i class="fas fa-home me-3"></i> D. Lauritzens vej 12, 6700 Esbjerg</br></br> 
+
+ 
+ 
+
+CVR-nr: 35975942</p> 
+
+          <p> 
+
+            <i class="fas fa-envelope me-3"></i> 
+
+            Sandramarialykke@gmail.com 
+
+          </p> 
+
+          <p><i class="fas fa-phone me-3"></i>+45 28 45 64 43</p> 
+
+          </p> 
+    </div>
+    <div class="col">
+	<p> 
+
+Følg os på sociale medier og modtag løbende opdateringer, nyheder, samt få mulighed for at deltage i konkurrencer <br> <br> 
+
+<a href="" class="me-4 text-reset"> 
+
+<i class="fab fa-facebook-f"></i> 
+
+</a> 
+
+<a href="" class="me-4 text-reset"> 
+
+<i class="fab fa-instagram"></i> 
+
+</a> 
+
+</p> 
+    </div>
+    <div class="col">
+      <p> 
+
+            <a href="#!" class="text-reset">Om VaneStærk</a> 
+
+          </p> 
+
+          <p> 
+
+            <a href="#!" class="text-reset">Forløb og priser</a> 
+
+          </p> 
+
+          <p> 
+
+            <a href="#!" class="text-reset">Booking</a> 
+
+          </p> 
+
+          <p> 
+
+            <a href="#!" class="text-reset">Kontakt</a> 
+
+          </p> 
+
+          <p> 
+
+            <a href="#!" class="text-reset">Shop</a> 
+
+          </p> 
+
+          <p> 
+
+            <a href="#!" class="text-reset">Privatlivspolitik</a> 
+
+          </p> 
+
+          <p> 
+
+            <a href="#!" class="text-reset">Retur politik</a> 
+
+          </p> 
+    </div>
+  </div>
+    © 2022 Copyright by
+    <a class="text-reset fw-bold" href="https://vanestaerk.dk">Vanestaerk.dk</a>
+  </div></div>
+  <!-- Copyright -->
 </footer>
 <!-- Footer -->
-
-</div><!-- #page -->
-
+<?php do_action( 'storefront_after_footer' ); ?>
 <?php wp_footer(); ?>
-
 </body>
 </html>
