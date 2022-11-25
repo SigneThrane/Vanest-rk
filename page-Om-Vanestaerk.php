1d9322eadbcf0e190e7c4e7450c44189e11b511d
SigneThrane <94352955+SigneThrane@users.noreply.github.com>
2022-11-25 19:26:36 +0100

    1

--- page-Om-Vanestaerk.php
+++ page-Om-Vanestaerk.php
@@ -63,9 +63,123 @@ get_header(); ?>
 </div>
 
             <div class="colcoral">
-                <h3>"Hvis du mestrer vanen, så kommer målet til dig.
-                    Men når du målet, uden at mestre vanen, så vil du miste målet igen på sigt." - Sandra Lykke</h3>
+                <h3>"Hvis du mestrer vanen, så kommer målet til dig. <br>
+                    Men når du målet, uden at mestre vanen, så vil du <br>
+                    miste målet igen på sigt." - Sandra Lykke</h3>
             </div>
+
+<div class="row">
+        <div class="col-6"id="venstreboks">
+            <p>I takt med min faglige og personlige <br>
+            udvikling, og især som følge af et <br>
+            større indblik i psykologien bag  <br>
+            spiseadfærd og vaneændring, har jeg <br>
+            følt et behov for at bryde ud af de <br>
+            kommercielle rammer. <br><br>
+            For det er ikke altid at et stort <br>
+            fitnesscenter, skaber de bedste  <br>
+            rammer for den mentale rejse, som en <br>
+            livsstilsændring jo også er. <br>
+            Tværtimod kan det være en barriere  <br>
+            for overhovedet at komme i gang. <br><br>
+            Derfor opstod motivationen for at <br>
+            skabe Vane|Stærk studiet også i mig. <br>
+            Et træningsstudie hvor der er plads til  <br>
+            at du kan være dig selv, helt uden at <br>
+            skulle forholde dig til andre. Det mener <br>
+            jeg giver de bedste rammer, for  <br>
+            arbejdet med vaneændring. Det er min <br>
+            klare erfaring og overbevisning, at: <br> <br>
+            Hvis du mestrer vanen, så kommer  <br>
+            målet til dig. Men når du målet, uden <br>
+            at mestre vanen, så vil du miste målet <br>
+            igen på sigt. <br><br>
+            Og i et forløb i Vane|Stærk studiet, vil <br>
+            du derfor arbejde med  <br>
+            vaneændring — og vil med stor <br>
+            sandsynlighed Ikke kun arbejde med  <br>
+            dine trænings- og spisevaner, men  <br>
+            også dykke ned i dine værdier og din <br>
+            indre dialog.
+        </p>
+        </div>
+
+            <div class="col-6" id="Corallinjeboks">
+        
+            <p>
+            <b>RELEVANT UDDANNELSE</b> <br>
+                <b>2021-2022</b> - Madmentor <br>
+                <b>201​1-2015</b> - Bachelor i Fy​sioterapi <br>
+                <b>2014</b> - Kostvejleder <br>
+                <b>2012</b>  ​- Personlig træner <br>
+                <b>2​010 </b>  - Fitness instruktør <br> <br>
+            
+                <b>MINI KURSER</b>
+           
+                <b>2021 </b> - Graviditet og efte​rfødsel v. Kristian Bradsted <br>
+                <b>2021 </b>   - Smerter v. Jacob B​eerman  <br>
+                <b>2017</b>    - Squat, bænkpres og​ dødløft v. Jacob Beerman <br> <br>
+            
+                  <b>ERFARING</b>
+          
+                <b> 2022</b>  - Åbning af Vane|Stæ​rk <br>
+                <b>2022</b> - Personlig træner i​ Fitness World Gl. Vardevej (fuldtid) <br>
+                <b>2018</b>  - Mentor for personl​ige trænere i Fitness World i region Syddanmark <br>
+               <b>2016</b>   - Personlig træner (​fuld tid) <br>
+                 <b> 2015</b>  - Ass. centerleder o​g personlig træner i FW Ådalsparken <br>
+                 <b>2012</b>  - Personlig træner i​ FW Ådalsparken (bijob) <br>
+                <b>2010</b>  - Fitness instruktør​ i FW Dannebrogsgade i Aalborg
+            </p>
+            <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+            </div>
+</div>
+
+        <div class="personale">
+            <hr class="rounded"><h1>OM OS <br>PERSONERNE HOS VANESTÆRK</h1><hr class="rounded">
+        </div>
+
+
+       <div class="row" id="personale1">
+        <div class="col-4">
+        <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+        </div>
+        
+                <div class="col-8">
+                    <h2>Sandra Lykke</h2>
+                    <p>
+                    Gruppetræning udgør en stor del af min praksis, da jeg har oplevet <br>
+                    hvordan træningsglæden og motivationen for at ​fastholde  <br>
+                    træningsrytmen stiger når vi samles om træningen i grupper. <br> <br>
+                    På gruppetræning indgår du på 1-2 hold ugentligt sammen med op <br>
+                    til 7 andre på hvert hold. Det er den samme gruppe du træner med <br>
+                    på hvert hold, så vi får oparbejdet en god dynamik og holdånd. <br> <br>
+                    Træningen består af helkropstræning med frie vægte og egen <br>
+                    kropsvægt. <br>
+                    Og vi arbejder med at fremme all round sundhed, med elementer af <br>
+                    styrke, smidighed og udholdenhed.
+                    </p>
+                </div>
+       </div> 
+
+       <div class="row" id="personale1">
+        <div class="col-4">
+        <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+        </div>
+        
+                       <div class="col-8">dfdf
+
+                     </div>
+       </div>
+
+       <div class="row" id="personale1">
+        <div class="col-4">
+        <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+        </div>
+        
+                     <div class="col-8">dfdf
+
+                    </div>
+       </div>
 
 </main>
 <?php
