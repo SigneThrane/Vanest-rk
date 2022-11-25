24f6c39cbd582f24025c17dfb16c87476fce32b8
AChristineP <94613529+AChristineP@users.noreply.github.com>
2022-11-25 19:26:17 +0100

    edit

--- page-kontakt.php
+++ page-kontakt.php
@@ -12,30 +12,35 @@
  */
 
 get_header(); ?>
-
+  
 <main id="main" class="site-main" role="main">
-     <div class="row-kontakt">
-             <div class="col4">
+
+<div class="container">
+        <div class="row" id="velkommen">
+        <div class="col-md-4">
+        <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+        </div>
+        <div class="col-md-8" id="col-text">
+             <div>
                   <br>
-                <h1>KONTAKT</h1>
+                  <hr class="rounded"><h1>KONTAKT</h1><hr>
                 <p>Vil du høre mere om mulighederne for personlig træning, <br> coaching eller gruppeforløb, så er jeg altid klar på en <br> uforpligtende snak over telefon eller i studiet. <br>
                     Du kan kontakte mig på telefon, instagram eller via <br> kontaktformularen her på siden.</p>
              </div>
-                      <div class="col5">
+                      <div id="banner">
                             <p>+45 28 45 64 43</p>
                             <p>@Sandras_Lykke</p>
                      </div>
-                    <div class="col6">
+                     </div>
+                    <div>
                           <h1>KONTAKTFORMULAR</h1>
                           <?php echo apply_shortcodes( '[contact-form-7 id="56" title="Contact form 1"]' ); ?>
 
                     </div>
-                       <div class="overlay">
-                       <img class="kontakt" src="<?php the_field('kontakt'); ?>" alt="" />
-                       <h1>Jeg ser frem til at <br> høre fra dig</h1>
-                       </div>
+     </div>
+        </div>
 
-    </div>
+
 
 </main>
 
