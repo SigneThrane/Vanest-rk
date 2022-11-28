5d0b3420a71eb31d54a6d225d74a0695cad5ed24
SigneThrane <signecathrane@gmail.com>
2022-11-28 11:25:30 +0100

    1

--- page-forlob-og-priser.php
+++ page-forlob-og-priser.php
@@ -31,8 +31,8 @@ get_header(); ?>
         Når du er i et forløb hos Vane|Stærk får du tilbud på <br> adgang til events, foredrag, temaaftener og workshops. <br><br>
         Klik herunder for at læse om: <br>
       </p>
-      <p>Individuelt forløb</p>
-      <p>Gruppetræning</p>
+     <a href="#"><p>Individuelt forløb</p></a> 
+       <a href="#"><p>Gruppetræning</p></a>   
 
      </div>
         </div>
@@ -40,7 +40,7 @@ get_header(); ?>
         <div class="grønboks">
         <div class="row">
         <div class="col-md-8">
-        <h2>INDIVIDUELT FORLØB</h2>
+        <h2 >INDIVIDUELT FORLØB</h2>
        <p>I et individuelt forløb modtager du privat træning og/eller coaching i <br>
        Vane|Stærk studiet. <br>
        Forløbet tager udgangspunkt i dine ønsker, og kan involvere både træning,  <br>
@@ -56,6 +56,251 @@ get_header(); ?>
       Når du er i et personlig træningsforløb, er det kun dig og mig, som er i studiet <br>
       under sessionerne.</p>
 </div>
+</div>
+</div>
+
+
+                     <div class="colforløb">
+                            <h2>FORLØBSTYPER</h2>
+                            <p> Vane|Stærk studiet tilbydes du forløb mellem 8 og 52 sessioner eller som abonnementsløsning.
+                                   <br> <br>
+                                   Herunder ser du en oversigt over aktuelle forløbspa​kker.
+                                   <br> <br>
+                                   Alle forløb kan købes via ratebetaling uden meromkost​n​ing.
+                            </p>
+                     </div>
+
+<table>
+        <tr>
+              <th>Mål</th>
+               <th>Antal sessioner</th>
+              <th>Kontant pris</th>
+               <th>Pr. Session</th>
+       </tr>
+                     <tr>
+                      <td>Kickstart</td>
+                      <td>8 sessioner</td>
+                      <td>5.200</td>
+                      <td>650</td>
+                     </tr>
+                            <tr>
+                             <td>Vaneændring</td>
+                             <td>16 sessioner</td>
+                             <td>10,000</td>
+                             <td>625</td>
+                            </tr>
+                                    <tr>
+                                     <td>Vaneændring og fastholdelse</td>
+                                     <td>32 sessioner</td>
+                                     <td>19.200</td>
+                                      <td>600</td>
+                                    </tr>
+                                                  <tr>
+                                                   <td>Vaneændring og fastholdelse</td>
+                                                  <td>52 sessioner</td>
+                                                   <td>29.900</td>
+                                                  <td>575</td>
+                                                   </tr>
+</table>
+
+<div class="colforløb">
+                            <h2>Abonnementsaftale</h2>
+                            <p> Skal personlig træning og coaching være en fast del af din livsstil, 
+                                   så du altid har en fast sparringspartner ved din side, til at hjælpe 
+                                   dig med at prioritere din sundhed og mentale trivsel, så kan du tegne 
+                                   et abonnement hos Vane|Stærk, hvor prisen for dine sessioner er reduceret 
+                                   til 595,- og ​du betaler en månedlig ydelse, svarende til antal bookede sessioner.
+                            </p>
+                     </div>
+
+<table>
+        <tr>
+              <th>Abonnementstype</th>
+               <th>1 session/uge</th>
+              <th>2 session/uge</th>
+       </tr>
+                     <tr>
+                      <td>Månedlig ydelse</td>
+                      <td>Forventeligt 2.400</td>
+                      <td>Forventeligt 4.800</td>
+                     </tr>
+                           
+</table>
+
+<div class="colforløb">
+          <p>
+          Ved oprettelse af abonnement binder du dig til minimum 3 måneder. (hhv. 16 eller 24 sessioner)
+          <br>
+          Samt binder dig til en opsigelsesperiode på 1 måned fra opsigelse.. (hhv 4 eller 8 sessioner)
+          </p>          
+</div>
+
+<div class="container">
+        <div class="row" id="beigeboks">
+        <div class="col-md-4">
+        <br>
+         <br>
+         <br>
+        <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+        </div>
+        <div class="col-md-8" id="col-text">
+        <br>
+         <br>
+         <br>
+        <hr class="rounded">
+        <h1>Jeg er altid til rådighed for en uforpligtende samtale inden køb af forløb.</h1>
+              <p>
+              Kontakt mig, så finder vi en tid hvor vi <br>
+              kan mødes og få en snak om dine <br>
+              ønsker og du kan blive klogere på, om <br>
+              det er mig ​der skal hjælpe dig. Eller <br>
+              find en ledig tid via "gå til booking" <br>
+              knappen herunder.
+              </p>
+           <a href="/booking/"> <button id="laes">Gå til booking</button></a>  
+     </div>
+        </div>
+
+        <div class="grønboks">
+        <div class="row">
+        <div class="col-md-8">
+        <h2>GRUPPETRÆNING</h2>
+              <p>
+              Gruppetræning udgør en stor del af min praksis, da jeg har oplevet 
+              hvordan træningsglæden og motivationen for at ​fastholde træningsrytmen 
+              stiger når vi samles om træningen i grupper. <br><br>
+              På gruppetræning indgår du på 1-2 hold ugentligt sammen med op til 7 
+              andre på hvert hold. Det er den samme gruppe du træner med på hvert hold, 
+              så vi får oparbejdet en god dynamik og holdånd. <br> <br>
+              Træningen består af helkropstræning med frie vægte og egen kropsvægt. 
+              Og vi arbejder med at fremme all round sundhed, med elementer af styrke, 
+              smidighed og udholdenhed.
+              </p>
+</div>
+</div>
+</div>
+
+<div class="colforløb">
+          <p>
+          Gruppetræningen udbydes som et abonnement, hvor du kan vælge om du vil deltage på 1 
+          eller 2 hold ugentligt. Tilkøb af abonnement sker ved henvendelse. <br> <br>
+          Herunder listet abonnementstyper og priser for gruppetræning:
+          </p>        
+</div>
+
+<table>
+        <tr>
+              <th>Abonnementstype</th>
+               <th>1 session/uge</th>
+              <th>2 session/uge</th>
+       </tr>
+                     <tr>
+                      <td>Månedlig ydelse</td>
+                      <td>275</td>
+                      <td>500</td>
+                     </tr>                       
+</table>
+
+<div class="colforløb">
+          <p>
+          Der er desuden mulighed for drop in, hvis der plads på holdene - pris 70 kr.
+               <br> <br>
+               Kontakt mig, hvis du vil have en besked, når der er ledige pladser.
+          </p>        
+</div>
+<div class="colforløb">
+              <h2>GRUPPETRÆNINGER</h2>
+              <p> I Vane|Stærk studiet tilbydes du forløb mellem 8 og 52 sessioner 
+                     eller som abonnementsløsning.
+              <br> <br>
+              Herunder ser du en oversigt over aktuelle forløbspa​kker.
+              <br> <br>
+              Alle forløb kan købes via ratebetaling uden meromkost​n​ing.
+                            </p>
+                     </div>
+
+
+<table>
+        <tr>
+              <th>Mandag</th>
+               <td>Stærke tøser: 16:30 <br> Fuld booket <br>(Sandra Lykke)</td>
+              <td>Stærke tøser: 18:00 <br>
+                     Ledig plads (7/8) <br>
+                     (Sandra Lykke)</td>
+               <td>Stærke tøser:19:15 <br>
+              Fuld booket <br>
+              (Sandra Lykke) <br></td>
+               <td></td>
+       </tr>
+                     <tr>
+                      <th>Tirsdag</th>
+                      <td>Stærke tøser: 9:00 <br>
+                     4 ledige pladser (4/8) <br>
+                     (Sandra Lykke)</td>
+                      <td>Stærke tøser: 16:30 <br>
+                     Nyt hold uge 40 <br>
+                     (Kristiane Schutt)</td>
+                      <td>Stærke tøser: 18:00 <br>
+                     Nyt hold uge 40 <br>
+                     (Kristiane Schutt)</td>
+                     <td></td>
+                     </tr>  
+                     </tr>
+                            <tr>
+                             <th>Onsdag</th>
+                             <td>Stærke tøser: 16:30 <br>
+                            Fuld booket <br>
+                            (Sandra Lykke)</td>
+                             <td>Stærke tøser: 18:00 <br>
+                            3 ledige pladser (5/8) <br>
+                            (Sara Bush)</td>
+                             <td>Stærke tøser: 19:15 <br>
+                                   Fuld booket <br>
+                                   (Sara Bush)</td>
+                             <td></td>
+                            </tr>
+                                    <tr>
+                                     <th>Torsdag</th>
+                                     <td>Stærke tøser: 9:30 <br>
+                                   3 ledige pladser (5/8) <br>
+                                   (Sandra Lykke)</td>
+                                     <td>Stærke tøser: 10:45 <br>
+                                   5 ledige pladser (3/8) <br>
+                                   (Sandra Lykke)</td>
+                                      <td>Stærke tøser: 18:30 <br> 
+                                          6 ledige pladser (2/8) <br>
+                                          (Sara Bush)</td>
+                                      <td>Stærke tøser: 19:15 <br>
+                                   2 ledige pladser (6/8) <br>
+                                   (Sara Bush)</td>
+                                    </tr>
+</table>
+
+<div class="container">
+        <div class="row" id="beigeboks">
+        <div class="col-md-4">
+        <br>
+         <br>
+         <br>
+        <img src="https://dummyimage.com/700x400/#FFE8D/0ffe8e.jpg&text=VaneSt%C3%A6rk" alt="">
+        </div>
+        <div class="col-md-8" id="col-text">
+        <br>
+         <br>
+         <br>
+        <hr class="rounded">
+       <p>Kontakt mig for at komme i gang som abonnent på gruppetræning i Vane|Stærk studiet.
+              <br>
+              
+Du kan også kontakte mig med ønsker om specifikke hold, eller hvis i er en gruppe som ønsker at træne sammen.
+       </p>
+            <a href="/kontakt/"><button id="laes">Kontakt</button></a>  
+     </div>
+        </div>
+
+
 </main>
+
+
 <?php
 get_footer();
